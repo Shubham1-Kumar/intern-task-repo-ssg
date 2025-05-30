ef44d4bc80ab720cda3a7ccccf7c79c5d85aafd5
siddhant <ishusingh@gmail.com>
2025-05-30 12:14:26 +0530

    n9

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import { createServer } from "http";
-import next from "next";
+const { createServer } = require("http");
+const next = require("next");
 
 const app = next({ dev: false });
 const handle = app.getRequestHandler();
